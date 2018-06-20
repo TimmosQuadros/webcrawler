4c20a41c9012749b983e5cd70fd13b075d2797e1
TimmosQuadros <timm.rasmussen@gmail.com>
2018-06-20 14:44:44 +0200

    Simple crawler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require ('express');
 var app = express();
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+var Crawler = require("crawler");
 
 
 //Connect to Mongoose
@@ -14,4 +15,36 @@ app.get('/',function(req,res){
 
 app.listen(3000);
 console.log('Running on port 3000');
+
+var c = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($("title").text());
+        }
+        done();
+    }
+});
+
+// Queue URLs with custom callbacks & parameters
+c.queue([{
+    uri: 'http://parishackers.org/',
+    jQuery: false,
+
+    // The global callback won't be called
+    callback: function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Grabbed', res.body.length, 'bytes');
+        }
+        done();
+    }
+}]);
 
