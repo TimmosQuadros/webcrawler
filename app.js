73d425cddb95dc882b4b2aae12f7fc92941027cc
TimmosQuadros <timm.rasmussen@gmail.com>
2018-06-20 14:12:30 +0200

    Simple express server.

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require ('express');
+var app = express();
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+//Connect to Mongoose
+mongoose.connect('');
+var db = mongoose.connection;
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Running on port 3000');
+
