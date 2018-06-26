4b7e7e3bc5cd8acdb1f8cdbe9b27fe1607db8557
TimmosQuadros <timm.rasmussen@gmail.com>
2018-06-26 21:16:14 +0000

    Webcrawler

--- app.js
+++ app.js
@@ -3,51 +3,120 @@ var app = express();
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var Crawler = require("crawler");
+var fs   = require('fs');
+var request = require('request');
+const puppeteer = require('puppeteer');
+
 
 
 //Connect to Mongoose
-mongoose.connect('');
+mongoose.connect('mongodb://localhost/myapp');
 var db = mongoose.connection;
 
 app.get('/',function(req,res){
     res.send('Hello World');
 });
 
-app.listen(3000);
-console.log('Running on port 3000');
+app.listen(3001);
+console.log('Running on port 3001');
 
-var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-            console.log($("title").text());
+puppeteer.launch({headless: true, args:['--no-sandbox']}).then(async browser => {
+    const page = await browser.newPage();
+    await page.goto('https://www.edc.dk/boligsalg/');
+    // other actions...
+    await delay(2000);
+    await page.click(".search-header__toggle-btn","left");
+    await delay(2000);
+    await page.click("#ContentContentPlaceHolder_NewHeader_repeaterContentData_ctl00_0_ctl00_0_ctl00_0_ctl00_0_ctl01_0_ctl00_0_buttonSearch_0","left");
+    await delay(2000);
+
+    //const selectElem = await page.$('#ContentContentPlaceHolder_MainContentPlaceHolder_ResultSortingAndItemsPerPage_DropDownListItemsPerPage');
+    //await selectElem.type('Value 1000');
+    //console.log(selectElem);
+
+    /*await page.evaluate(() => {
+        document.querySelector('#ContentContentPlaceHolder_MainContentPlaceHolder_ResultSortingAndItemsPerPage_DropDownListItemsPerPage option:nth-child(2)').selected = true;
+    });*/
+
+    /*const option = (await page.$x(
+        '//*[@id = "ContentContentPlaceHolder_MainContentPlaceHolder_ResultSortingAndItemsPerPage_DropDownListItemsPerPage"]/option[text() = "Alle"]'
+    ))[0];
+    const value = await (await option.getProperty('value')).jsonValue();*/
+
+    await page.select('#ContentContentPlaceHolder_MainContentPlaceHolder_ResultSortingAndItemsPerPage_DropDownListItemsPerPage', "1000");
+    await delay(5000);
+    //await page.screenshot({path: 'example.png'});
+    const imgs = await page.$$eval('.propertyitem__img', imgs => imgs.map(img => img.getAttribute('data-src')));
+    const alts = await page.$$eval('.propertyitem__img', imgs => imgs.map(img => img.getAttribute('alt').replace(',',"")));
+    var i = 0;
+    imgs.forEach(function(element) {
+        element = element.replace("_Size210x140","_Size1920x1080");
+        if(element.includes("_Size300x200")){
+            element = element.replace("_Size300x200","_Size1920x1080");
         }
-        done();
-    }
+        if(element.includes("billeder.edc.dk")) {
+            if (element.substring(0, 2).localeCompare("//") == 0) {
+                console.log(element);
+                download("http:"+element, alts[i], 'images', function () {
+                    //console.log('downloaded');
+                });
+            } else {
+                console.log(element);
+                download(element, alts[i], 'images', function () {
+                    //console.log('downloaded');
+                });
+            }
+        }
+        i++;
+    });
+    /*imgs.forEach(function(element) {
+
+    });*/
+
+    //console.log("http:".substring(0,5).localeCompare("http:")==0);
+
+    //console.log(imgs.length);
+    console.log("Done");
 });
 
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    });
+}
 
-// Queue URLs with custom callbacks & parameters
-c.queue([{
-    uri: 'https://www.boligsiden.dk/',
-    // The global callback won't be called
-    callback: function (error, res, done) {
+function writeImageFile(imageData, fname){
+    var imagedir = 'imagedir';
+
+    fs.stat(imagedir, function(error, stat){
         if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
-            //console.log('Grabbed', res.body.length, 'bytes');
-            var button = $("button:contains('Søg')");
-            //console.log(pages);
-            console.log(button.className);
+            fs.mkdirSync(imagedir, 0766);
         }
-        done();
-    }
-}]);
+        fs.writeFile(imagedir + '/' + fname, imageData, 'binary', function(error){
+            if(error){
+                console.log(error);
+            }
+        });
+    });
+}
+
+var download = function(uri, filename, imagedir, callback) {
+    fs.stat(imagedir, function(error, stat){
+        if(error){
+            fs.mkdirSync(imagedir, 0766);
+        }
+    });
+
+    // make the filename not need a directory
+    var file = filename.split('/')[filename.split('/').length - 1];
+
+    request.head(uri, function(err, res, body) {
+        var r = request(uri).pipe(fs.createWriteStream(imagedir +'/'+ file+".jpg"));
+        r.on('close', callback);
+        r.on('error', callback);
+    });
+};
+
+
+
 
